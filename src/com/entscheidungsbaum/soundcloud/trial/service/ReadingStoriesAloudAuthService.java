1c57fdf97371b786a92a3803c40330b190cbcb1c
marcus <marcus.schlechter@yahoo.de>
2012-05-02 00:45:20 +0200

    token , logo etc.

--- src/com/entscheidungsbaum/soundcloud/trial/service/ReadingStoriesAloudAuthService.java
+++ src/com/entscheidungsbaum/soundcloud/trial/service/ReadingStoriesAloudAuthService.java
@@ -8,43 +8,52 @@ import android.os.IBinder;
 import android.util.Log;
 import com.soundcloud.api.ApiWrapper;
 import com.soundcloud.api.Env;
+import com.soundcloud.api.Token;
 
 /**
  * marcus SoundCloudTrial
- * 
+ * <p/>
  * 30.04.2012
- **/
+ */
 public class ReadingStoriesAloudAuthService extends Service {
 
     final String LOG_TAG = "ReadingStoriesAloudAuthService";
 
-	final String CLIENT_ID = "d88a5f695232355d8413e8392a8ecb21";
-	final String CLIENT_SECRET = "d88a5f695232355d8413e8392a8ecb21";
+    final String CLIENT_ID = "d88a5f695232355d8413e8392a8ecb21";
+    final String CLIENT_SECRET = "c8ee69e777a64346401cbb4c8c4cf1d1";
 
 
-	final String END_USER_AUTHORIZATON_URL = "https://soundcloud.com/connect";
-	final String TOKEN = "https://api.soundcloud.com/oauth2/token";
-	
-	final String TAG="";
+    final String END_USER_AUTHORIZATON_URL = "https://soundcloud.com/connect";
+    final String TOKEN = "https://api.soundcloud.com/oauth2/token";
 
-    AccountManager mAccountManager ;
+    final String TAG = "";
+
+    AccountManager mAccountManager;
     Account mAccount;
 
+
     ApiWrapper mWrapper;
+    Token token;
 
-    public void authenticate(){
-        String username ="marcus.schlechter@yahoo.de";
-        String passwd ="linus123";
-        mWrapper = new ApiWrapper(CLIENT_ID,CLIENT_SECRET, null, null, Env.LIVE);
-        Log.d(LOG_TAG, " getToken in authenticate" + mWrapper.getToken());
+    public void authenticate() throws Exception {
+        String username = "marcus.schlechter@yahoo.de";
+        String passwd = "linus123";
+        mWrapper = new ApiWrapper(CLIENT_ID, CLIENT_SECRET, null, null, Env.SANDBOX);
+        Log.d(LOG_TAG, "got a wrapper");
+        if (mWrapper.debugRequests) {
+            Log.d(LOG_TAG, "Debug request");
+        }
 
+        token = mWrapper.login(username, passwd, Env.SANDBOX.toString());
+
+        Log.d(LOG_TAG, " getToken in authenticate" +token);
 
     }
 
-	@Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
